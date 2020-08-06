c6d0f1eaed5fc238c7ec5bcd141e6f8a438455fd
Luis Martinez <devluismartinezr@gmail.com>
2020-08-06 01:29:25 -0500

    post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Carbon\Carbon;
 
 class Post extends Model
 {
-    //
+    //Applying soft deletes to keep the data into the database after delete
+    use SoftDeletes;
+
+    protected $table = 'posts';
+    protected $fillable = ['title','description','publication_date','user_id'];
+
+
+    public function user(){
+
+        return $this->belongsTo(User::class);
+    }   
+    
+    public function setPublicationDateAttribute($value){
+        
+        $this->attributes['publication_date'] = Carbon::now()->format('Y-m-d H:i:s');
+    }
+
+    public function setUserIdAttribute($value)
+    {   
+        $this->attributes['user_id'] = auth()->id();
+    }
 }
---
+++ database/migrations/2020_08_06_002334_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('description', 255)->nullable();
+            $table->datetime('publication_date');
+            $table->foreignId('user_id')->constrained('users');//relationship with user
+            $table->timestamps();
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
